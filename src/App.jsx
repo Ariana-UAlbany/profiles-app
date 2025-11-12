b6ea9f06d3f9569153d9c89549d54d41c8143c42
Ariana-UAlbany <acnieves@albany.edu>
2025-11-12 04:49:29 +0000

    part3: add components props and .map()

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,26 @@
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import ProfileCard from './components/ProfileCard.jsx';
+import { profiles } from './data/profiles.js';
+
+export default function App() {
+  return (
+    <Container className="py-4">
+      <h1 className="mb-4 text-center">Profiles</h1>
+      <Row xs={1} md={2} lg={3}>
+        {profiles.map(p => (
+          <Col key={p.id}>
+            <ProfileCard name={p.name} likes={p.likes} />
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+}
+
+/*
+import Container from 'react-bootstrap/Container';
 import Alert from 'react-bootstrap/Alert';
 
 export default function App() {
@@ -10,6 +32,7 @@ export default function App() {
     </Container>
   );
 }
+*/
 /*
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
