79c8b3386f7b1bf7a94faef41c0b0d594b610981
Ariana-UAlbany <acnieves@albany.edu>
2025-11-12 05:32:26 +0000

    part4: Stateful UI and Like Button

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,22 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import ProfileCard from './components/ProfileCard.jsx';
 import { profiles } from './data/profiles.js';
+import { useState } from 'react';
 
 export default function App() {
+  const [people, setPeople] = useState(profiles);
+  function incrementLikes(id){
+    setPeople(ps => ps.map(p => p.id===id? { ...p, likes: p.likes+1 } : p));
+  }
+
   return (
-    <Container className="py-4">
+    <Container fluid className="py-4">
       <h1 className="mb-4 text-center">Profiles</h1>
       <Row xs={1} md={2} lg={3}>
-        {profiles.map(p => (
+        {people.map(p => (
           <Col key={p.id}>
-            <ProfileCard name={p.name} likes={p.likes} />
+            <ProfileCard name={p.name} likes={p.likes} 
+            likeButton={()=>incrementLikes(p.id)}/>
           </Col>
         ))}
       </Row>
